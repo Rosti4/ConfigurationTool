b0449c08a6c60777d8b48e824efe4c1fb41309f9
Stephan <stephan.rothschaedl@gmail.com>
2016-03-25 15:01:58 +0100

    Initial commit

---
+++ app/src/androidTest/java/at/campus02/atb13/rothschaedl/ssiconfigurationtool/ApplicationTest.java
@@ -0,0 +1,13 @@
+package at.campus02.atb13.rothschaedl.ssiconfigurationtool;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
